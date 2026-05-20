7f2c5fe6db3abb601c8772c5fdf63881e62ec4b6
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-20 21:57:22 +0600

    Refactor main.dart initialization and update dependency imports
    - Remove direct `FirebaseAuth` and `GoogleSignIn` instances and unused imports.
    - Add imports for `AuthController`, `AppRouter`, and `SpService`.
    - Add commented-out boilerplate for registering `AuthController` and `AppRouter` singletons using `getIt` to prepare for updated dependency injection logic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:blood_setu/application/core/auth/auth_controller.dart';
+import 'package:blood_setu/application/core/services/routing/app_router.dart';
+import 'package:blood_setu/application/core/services/sp_service/sp_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'application/pages/app/app.dart';
 import 'di/di.dart';
@@ -14,10 +15,14 @@ void main() async {
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn.instance;
-  // await Future.wait([firebaseAuth.signOut(), googleSignIn.signOut()]);
 
   await configureDependencies();
+
+  // final authController = AuthController(getIt<SpService>());
+  // final appRouter = AppRouter(authController);
+  //
+  // getIt.registerSingleton<AuthController>(authController);
+  // getIt.registerSingleton<AppRouter>(appRouter);
+
   runApp(const BloodConnectApp());
 }
