d59c380d67272978e7c6dafe8abfb177f852952a
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-24 11:36:45 +0600

    [sign_in] Remove unused imports in SignInScreen
    Removes unused imports for the `Screen` domain model and navigation BLoC classes in `sign_in_screen.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:blood_setu/application/core/auth/auth_controller.dart';
-import 'package:blood_setu/application/core/services/routing/app_router.dart';
-import 'package:blood_setu/application/core/services/sp_service/sp_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,11 +15,6 @@ void main() async {
 
   await configureDependencies();
 
-  // final authController = AuthController(getIt<SpService>());
-  // final appRouter = AppRouter(authController);
-  //
-  // getIt.registerSingleton<AuthController>(authController);
-  // getIt.registerSingleton<AppRouter>(appRouter);
 
   runApp(const BloodConnectApp());
 }
