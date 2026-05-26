c34c05bdf1522318e4a7be58c927e04325cc9f16
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-26 21:50:40 +0600

    Add BottomNavState to manage bottom navigation state

---
+++ lib/application/pages/features/bottom_nav/bloc/bottom_nav_state.dart
@@ -0,0 +1,10 @@
+part of 'bottom_nav_bloc.dart';
+
+@freezed
+class BottomNavState with _$BottomNavState {
+  const factory BottomNavState({
+    @Default(0) int currentIndex,
+  }) = _BottomNavState;
+
+  factory BottomNavState.initial() => const BottomNavState();
+}
