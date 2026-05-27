d4d9e6ca5f6dd492374d3cbfd183cbb9ad2ff6e1
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-27 21:27:04 +0600

    [home] Add UserProfileModel to HomeState
    Adds an optional `UserProfileModel` field to the `HomeState` to allow the home feature to manage and display user profile information.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [AI contribution guidelines] and understand my
    responsibilities, or I am not using AI tools.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.

--- lib/application/pages/features/home/bloc/home_state.dart
+++ lib/application/pages/features/home/bloc/home_state.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../../../../domain/models/user_profile_model.dart';
+
 part 'home_state.freezed.dart';
 
 @freezed
@@ -7,6 +9,7 @@ class HomeState with _$HomeState {
   const factory HomeState({
     @Default(false) bool showSidebar,
     @Default(false) bool sosPressed,
+    UserProfileModel? profile,
   }) = _HomeState;
 
   factory HomeState.initial() => const HomeState();
