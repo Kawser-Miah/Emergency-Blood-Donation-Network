bba2d4b741674ef2078b63f6da0974cfb66d5d7b
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-27 21:27:28 +0600

    [home] Refactor HomeBloc to integrate profile fetching on startup

--- lib/application/pages/features/home/bloc/home_bloc.dart
+++ lib/application/pages/features/home/bloc/home_bloc.dart
@@ -1,12 +1,29 @@
+import 'package:blood_setu/application/core/auth/auth_controller.dart';
+import 'package:blood_setu/di/di.dart';
+import 'package:blood_setu/domain/usecase/registration_user_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
+import '../../../../../domain/usecase/authentication_usecase.dart';
 import 'home_event.dart';
 import 'home_state.dart';
 
+@injectable
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeState.initial()) {
+  final RegistrationUserUseCase _registrationUserUseCase;
+
+  HomeBloc(this._registrationUserUseCase) : super(HomeState.initial()) {
     on<HomeEvent>((event, emit) async {
       await event.when(
+        started: () async {
+          final uid = getIt<AuthController>().user?.uid;
+          if (uid == null) return;
+          final result = await _registrationUserUseCase.getProfile(uid);
+          result.fold(
+            (_) {},
+            (profile) => emit(state.copyWith(profile: profile)),
+          );
+        },
         sidebarOpened: () async => emit(state.copyWith(showSidebar: true)),
         sidebarClosed: () async => emit(state.copyWith(showSidebar: false)),
         sosPressed: () async {
