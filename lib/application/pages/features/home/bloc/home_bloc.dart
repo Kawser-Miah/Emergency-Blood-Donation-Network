f299c9bddb405a893ca4e3d37b09afb83496c304
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-30 12:37:38 +0600

    Refactor HomeBloc to include LocationUseCase and improve event handling

--- lib/application/pages/features/home/bloc/home_bloc.dart
+++ lib/application/pages/features/home/bloc/home_bloc.dart
@@ -1,28 +1,32 @@
+import 'dart:async';
+
 import 'package:blood_setu/application/core/auth/auth_controller.dart';
 import 'package:blood_setu/di/di.dart';
+import 'package:blood_setu/domain/usecase/location_usecase.dart';
 import 'package:blood_setu/domain/usecase/registration_user_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
-import '../../../../../domain/usecase/authentication_usecase.dart';
 import 'home_event.dart';
 import 'home_state.dart';
 
 @injectable
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final RegistrationUserUseCase _registrationUserUseCase;
+  final LocationUseCase _locationUseCase;
 
-  HomeBloc(this._registrationUserUseCase) : super(HomeState.initial()) {
+  HomeBloc(this._registrationUserUseCase, this._locationUseCase)
+    : super(HomeState.initial()) {
     on<HomeEvent>((event, emit) async {
       await event.when(
         started: () async {
           final uid = getIt<AuthController>().user?.uid;
           if (uid == null) return;
+
           final result = await _registrationUserUseCase.getProfile(uid);
-          result.fold(
-            (_) {},
-            (profile) => emit(state.copyWith(profile: profile)),
-          );
+          result.fold((_) {}, (profile) => emit(state.copyWith(profile: profile)));
+
+          unawaited(_locationUseCase.updateLocation(uid));
         },
         sidebarOpened: () async => emit(state.copyWith(showSidebar: true)),
         sidebarClosed: () async => emit(state.copyWith(showSidebar: false)),
