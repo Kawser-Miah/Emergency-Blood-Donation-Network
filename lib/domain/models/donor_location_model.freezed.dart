394a3418f58ddcfd71107faefbcdced2a8f6ba15
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-31 13:17:36 +0600

    Add DonorLocationModel with additional fields for enhanced donor data management

---
+++ lib/domain/models/donor_location_model.freezed.dart
@@ -0,0 +1,414 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'donor_location_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$DonorLocationModel {
+  String get uid => throw _privateConstructorUsedError;
+  String get fullName => throw _privateConstructorUsedError;
+  String get bloodGroup => throw _privateConstructorUsedError;
+  String get district => throw _privateConstructorUsedError;
+  String get thana => throw _privateConstructorUsedError;
+  bool get isActive => throw _privateConstructorUsedError;
+  String get donorTier => throw _privateConstructorUsedError;
+  int get totalDonations => throw _privateConstructorUsedError;
+  String? get photoUrl => throw _privateConstructorUsedError;
+  double? get latitude => throw _privateConstructorUsedError;
+  double? get longitude => throw _privateConstructorUsedError;
+  String? get geohash => throw _privateConstructorUsedError;
+
+  /// Create a copy of DonorLocationModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $DonorLocationModelCopyWith<DonorLocationModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DonorLocationModelCopyWith<$Res> {
+  factory $DonorLocationModelCopyWith(
+    DonorLocationModel value,
+    $Res Function(DonorLocationModel) then,
+  ) = _$DonorLocationModelCopyWithImpl<$Res, DonorLocationModel>;
+  @useResult
+  $Res call({
+    String uid,
+    String fullName,
+    String bloodGroup,
+    String district,
+    String thana,
+    bool isActive,
+    String donorTier,
+    int totalDonations,
+    String? photoUrl,
+    double? latitude,
+    double? longitude,
+    String? geohash,
+  });
+}
+
+/// @nodoc
+class _$DonorLocationModelCopyWithImpl<$Res, $Val extends DonorLocationModel>
+    implements $DonorLocationModelCopyWith<$Res> {
+  _$DonorLocationModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of DonorLocationModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? uid = null,
+    Object? fullName = null,
+    Object? bloodGroup = null,
+    Object? district = null,
+    Object? thana = null,
+    Object? isActive = null,
+    Object? donorTier = null,
+    Object? totalDonations = null,
+    Object? photoUrl = freezed,
+    Object? latitude = freezed,
+    Object? longitude = freezed,
+    Object? geohash = freezed,
+  }) {
+    return _then(
+      _value.copyWith(
+            uid: null == uid
+                ? _value.uid
+                : uid // ignore: cast_nullable_to_non_nullable
+                      as String,
+            fullName: null == fullName
+                ? _value.fullName
+                : fullName // ignore: cast_nullable_to_non_nullable
+                      as String,
+            bloodGroup: null == bloodGroup
+                ? _value.bloodGroup
+                : bloodGroup // ignore: cast_nullable_to_non_nullable
+                      as String,
+            district: null == district
+                ? _value.district
+                : district // ignore: cast_nullable_to_non_nullable
+                      as String,
+            thana: null == thana
+                ? _value.thana
+                : thana // ignore: cast_nullable_to_non_nullable
+                      as String,
+            isActive: null == isActive
+                ? _value.isActive
+                : isActive // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            donorTier: null == donorTier
+                ? _value.donorTier
+                : donorTier // ignore: cast_nullable_to_non_nullable
+                      as String,
+            totalDonations: null == totalDonations
+                ? _value.totalDonations
+                : totalDonations // ignore: cast_nullable_to_non_nullable
+                      as int,
+            photoUrl: freezed == photoUrl
+                ? _value.photoUrl
+                : photoUrl // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            latitude: freezed == latitude
+                ? _value.latitude
+                : latitude // ignore: cast_nullable_to_non_nullable
+                      as double?,
+            longitude: freezed == longitude
+                ? _value.longitude
+                : longitude // ignore: cast_nullable_to_non_nullable
+                      as double?,
+            geohash: freezed == geohash
+                ? _value.geohash
+                : geohash // ignore: cast_nullable_to_non_nullable
+                      as String?,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$DonorLocationModelImplCopyWith<$Res>
+    implements $DonorLocationModelCopyWith<$Res> {
+  factory _$$DonorLocationModelImplCopyWith(
+    _$DonorLocationModelImpl value,
+    $Res Function(_$DonorLocationModelImpl) then,
+  ) = __$$DonorLocationModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    String uid,
+    String fullName,
+    String bloodGroup,
+    String district,
+    String thana,
+    bool isActive,
+    String donorTier,
+    int totalDonations,
+    String? photoUrl,
+    double? latitude,
+    double? longitude,
+    String? geohash,
+  });
+}
+
+/// @nodoc
+class __$$DonorLocationModelImplCopyWithImpl<$Res>
+    extends _$DonorLocationModelCopyWithImpl<$Res, _$DonorLocationModelImpl>
+    implements _$$DonorLocationModelImplCopyWith<$Res> {
+  __$$DonorLocationModelImplCopyWithImpl(
+    _$DonorLocationModelImpl _value,
+    $Res Function(_$DonorLocationModelImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of DonorLocationModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? uid = null,
+    Object? fullName = null,
+    Object? bloodGroup = null,
+    Object? district = null,
+    Object? thana = null,
+    Object? isActive = null,
+    Object? donorTier = null,
+    Object? totalDonations = null,
+    Object? photoUrl = freezed,
+    Object? latitude = freezed,
+    Object? longitude = freezed,
+    Object? geohash = freezed,
+  }) {
+    return _then(
+      _$DonorLocationModelImpl(
+        uid: null == uid
+            ? _value.uid
+            : uid // ignore: cast_nullable_to_non_nullable
+                  as String,
+        fullName: null == fullName
+            ? _value.fullName
+            : fullName // ignore: cast_nullable_to_non_nullable
+                  as String,
+        bloodGroup: null == bloodGroup
+            ? _value.bloodGroup
+            : bloodGroup // ignore: cast_nullable_to_non_nullable
+                  as String,
+        district: null == district
+            ? _value.district
+            : district // ignore: cast_nullable_to_non_nullable
+                  as String,
+        thana: null == thana
+            ? _value.thana
+            : thana // ignore: cast_nullable_to_non_nullable
+                  as String,
+        isActive: null == isActive
+            ? _value.isActive
+            : isActive // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        donorTier: null == donorTier
+            ? _value.donorTier
+            : donorTier // ignore: cast_nullable_to_non_nullable
+                  as String,
+        totalDonations: null == totalDonations
+            ? _value.totalDonations
+            : totalDonations // ignore: cast_nullable_to_non_nullable
+                  as int,
+        photoUrl: freezed == photoUrl
+            ? _value.photoUrl
+            : photoUrl // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        latitude: freezed == latitude
+            ? _value.latitude
+            : latitude // ignore: cast_nullable_to_non_nullable
+                  as double?,
+        longitude: freezed == longitude
+            ? _value.longitude
+            : longitude // ignore: cast_nullable_to_non_nullable
+                  as double?,
+        geohash: freezed == geohash
+            ? _value.geohash
+            : geohash // ignore: cast_nullable_to_non_nullable
+                  as String?,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$DonorLocationModelImpl extends _DonorLocationModel {
+  const _$DonorLocationModelImpl({
+    required this.uid,
+    this.fullName = '',
+    this.bloodGroup = '',
+    this.district = '',
+    this.thana = '',
+    this.isActive = true,
+    this.donorTier = '',
+    this.totalDonations = 0,
+    this.photoUrl,
+    this.latitude,
+    this.longitude,
+    this.geohash,
+  }) : super._();
+
+  @override
+  final String uid;
+  @override
+  @JsonKey()
+  final String fullName;
+  @override
+  @JsonKey()
+  final String bloodGroup;
+  @override
+  @JsonKey()
+  final String district;
+  @override
+  @JsonKey()
+  final String thana;
+  @override
+  @JsonKey()
+  final bool isActive;
+  @override
+  @JsonKey()
+  final String donorTier;
+  @override
+  @JsonKey()
+  final int totalDonations;
+  @override
+  final String? photoUrl;
+  @override
+  final double? latitude;
+  @override
+  final double? longitude;
+  @override
+  final String? geohash;
+
+  @override
+  String toString() {
+    return 'DonorLocationModel(uid: $uid, fullName: $fullName, bloodGroup: $bloodGroup, district: $district, thana: $thana, isActive: $isActive, donorTier: $donorTier, totalDonations: $totalDonations, photoUrl: $photoUrl, latitude: $latitude, longitude: $longitude, geohash: $geohash)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DonorLocationModelImpl &&
+            (identical(other.uid, uid) || other.uid == uid) &&
+            (identical(other.fullName, fullName) ||
+                other.fullName == fullName) &&
+            (identical(other.bloodGroup, bloodGroup) ||
+                other.bloodGroup == bloodGroup) &&
+            (identical(other.district, district) ||
+                other.district == district) &&
+            (identical(other.thana, thana) || other.thana == thana) &&
+            (identical(other.isActive, isActive) ||
+                other.isActive == isActive) &&
+            (identical(other.donorTier, donorTier) ||
+                other.donorTier == donorTier) &&
+            (identical(other.totalDonations, totalDonations) ||
+                other.totalDonations == totalDonations) &&
+            (identical(other.photoUrl, photoUrl) ||
+                other.photoUrl == photoUrl) &&
+            (identical(other.latitude, latitude) ||
+                other.latitude == latitude) &&
+            (identical(other.longitude, longitude) ||
+                other.longitude == longitude) &&
+            (identical(other.geohash, geohash) || other.geohash == geohash));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    uid,
+    fullName,
+    bloodGroup,
+    district,
+    thana,
+    isActive,
+    donorTier,
+    totalDonations,
+    photoUrl,
+    latitude,
+    longitude,
+    geohash,
+  );
+
+  /// Create a copy of DonorLocationModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DonorLocationModelImplCopyWith<_$DonorLocationModelImpl> get copyWith =>
+      __$$DonorLocationModelImplCopyWithImpl<_$DonorLocationModelImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _DonorLocationModel extends DonorLocationModel {
+  const factory _DonorLocationModel({
+    required final String uid,
+    final String fullName,
+    final String bloodGroup,
+    final String district,
+    final String thana,
+    final bool isActive,
+    final String donorTier,
+    final int totalDonations,
+    final String? photoUrl,
+    final double? latitude,
+    final double? longitude,
+    final String? geohash,
+  }) = _$DonorLocationModelImpl;
+  const _DonorLocationModel._() : super._();
+
+  @override
+  String get uid;
+  @override
+  String get fullName;
+  @override
+  String get bloodGroup;
+  @override
+  String get district;
+  @override
+  String get thana;
+  @override
+  bool get isActive;
+  @override
+  String get donorTier;
+  @override
+  int get totalDonations;
+  @override
+  String? get photoUrl;
+  @override
+  double? get latitude;
+  @override
+  double? get longitude;
+  @override
+  String? get geohash;
+
+  /// Create a copy of DonorLocationModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$DonorLocationModelImplCopyWith<_$DonorLocationModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
