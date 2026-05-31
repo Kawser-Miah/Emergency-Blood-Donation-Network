8a1e5e894f99218488a9f97e9e1aaaf1678c1d38
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-05-31 13:18:28 +0600

    Add NearbyDonorsUseCase to handle donor retrieval logic

---
+++ lib/domain/usecase/nearby_donors_usecase.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+import '../failures/failures.dart';
+import '../models/nearby_donor.dart';
+import '../repositories/nearby_donors_repository.dart';
+
+@injectable
+class NearbyDonorsUseCase {
+  final NearbyDonorsRepository _repository;
+
+  NearbyDonorsUseCase(this._repository);
+
+  Future<Either<Failure, ({double latitude, double longitude})>> getOrigin(
+    String uid,
+  ) => _repository.getOrigin(uid);
+
+  Future<Either<Failure, List<NearbyDonor>>> call({
+    required double latitude,
+    required double longitude,
+    required double radiusKm,
+    String? bloodGroup,
+    String? excludeUid,
+  }) => _repository.getNearbyDonors(
+    latitude: latitude,
+    longitude: longitude,
+    radiusKm: radiusKm,
+    bloodGroup: bloodGroup,
+    excludeUid: excludeUid,
+  );
+}
