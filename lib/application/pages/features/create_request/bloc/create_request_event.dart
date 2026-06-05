c974987a1c260e4c3a2aecbb3d54a217a37f8090
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-05 19:38:37 +0600

    Update CreateRequestEvent to change needByChanged type to DateTime and add new events for GPS location request and request submission

--- lib/application/pages/features/create_request/bloc/create_request_event.dart
+++ lib/application/pages/features/create_request/bloc/create_request_event.dart
@@ -11,10 +11,11 @@ class CreateRequestEvent with _$CreateRequestEvent {
   const factory CreateRequestEvent.hospitalChanged(String value) = _HospitalChanged;
   const factory CreateRequestEvent.addressChanged(String value) = _AddressChanged;
   const factory CreateRequestEvent.urgencyChanged(String value) = _UrgencyChanged;
-  const factory CreateRequestEvent.needByChanged(String value) = _NeedByChanged;
+  const factory CreateRequestEvent.needByChanged(DateTime value) = _NeedByChanged;
   const factory CreateRequestEvent.contactChanged(String value) = _ContactChanged;
   const factory CreateRequestEvent.notesChanged(String value) = _NotesChanged;
-  const factory CreateRequestEvent.shareFacebookToggled() = _ShareFacebookToggled;
   const factory CreateRequestEvent.confirmed1Toggled() = _Confirmed1Toggled;
   const factory CreateRequestEvent.confirmed2Toggled() = _Confirmed2Toggled;
+  const factory CreateRequestEvent.gpsLocationRequested() = _GpsLocationRequested;
+  const factory CreateRequestEvent.requestSubmitted() = _RequestSubmitted;
 }
