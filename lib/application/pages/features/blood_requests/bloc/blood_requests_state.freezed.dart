901586eef8284553766566b69c0b58183b11e130
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-07 13:42:01 +0600

    Add BloodRequestsState implementation with additional fields and copyWith method

---
+++ lib/application/pages/features/blood_requests/bloc/blood_requests_state.freezed.dart
@@ -0,0 +1,426 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'blood_requests_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$BloodRequestsState {
+  List<BloodRequest> get requests => throw _privateConstructorUsedError;
+  List<BloodRequest> get filtered => throw _privateConstructorUsedError;
+  bool get hasMore => throw _privateConstructorUsedError;
+  bool get isLoading => throw _privateConstructorUsedError;
+  bool get isLoadingMore => throw _privateConstructorUsedError;
+  String get search => throw _privateConstructorUsedError;
+  String get selectedBloodGroup => throw _privateConstructorUsedError;
+  String get selectedUrgency => throw _privateConstructorUsedError;
+  bool get showFilters => throw _privateConstructorUsedError;
+  String? get error => throw _privateConstructorUsedError;
+  double? get userLat => throw _privateConstructorUsedError;
+  double? get userLng => throw _privateConstructorUsedError;
+
+  /// Create a copy of BloodRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $BloodRequestsStateCopyWith<BloodRequestsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BloodRequestsStateCopyWith<$Res> {
+  factory $BloodRequestsStateCopyWith(
+    BloodRequestsState value,
+    $Res Function(BloodRequestsState) then,
+  ) = _$BloodRequestsStateCopyWithImpl<$Res, BloodRequestsState>;
+  @useResult
+  $Res call({
+    List<BloodRequest> requests,
+    List<BloodRequest> filtered,
+    bool hasMore,
+    bool isLoading,
+    bool isLoadingMore,
+    String search,
+    String selectedBloodGroup,
+    String selectedUrgency,
+    bool showFilters,
+    String? error,
+    double? userLat,
+    double? userLng,
+  });
+}
+
+/// @nodoc
+class _$BloodRequestsStateCopyWithImpl<$Res, $Val extends BloodRequestsState>
+    implements $BloodRequestsStateCopyWith<$Res> {
+  _$BloodRequestsStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of BloodRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? requests = null,
+    Object? filtered = null,
+    Object? hasMore = null,
+    Object? isLoading = null,
+    Object? isLoadingMore = null,
+    Object? search = null,
+    Object? selectedBloodGroup = null,
+    Object? selectedUrgency = null,
+    Object? showFilters = null,
+    Object? error = freezed,
+    Object? userLat = freezed,
+    Object? userLng = freezed,
+  }) {
+    return _then(
+      _value.copyWith(
+            requests: null == requests
+                ? _value.requests
+                : requests // ignore: cast_nullable_to_non_nullable
+                      as List<BloodRequest>,
+            filtered: null == filtered
+                ? _value.filtered
+                : filtered // ignore: cast_nullable_to_non_nullable
+                      as List<BloodRequest>,
+            hasMore: null == hasMore
+                ? _value.hasMore
+                : hasMore // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            isLoading: null == isLoading
+                ? _value.isLoading
+                : isLoading // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            isLoadingMore: null == isLoadingMore
+                ? _value.isLoadingMore
+                : isLoadingMore // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            search: null == search
+                ? _value.search
+                : search // ignore: cast_nullable_to_non_nullable
+                      as String,
+            selectedBloodGroup: null == selectedBloodGroup
+                ? _value.selectedBloodGroup
+                : selectedBloodGroup // ignore: cast_nullable_to_non_nullable
+                      as String,
+            selectedUrgency: null == selectedUrgency
+                ? _value.selectedUrgency
+                : selectedUrgency // ignore: cast_nullable_to_non_nullable
+                      as String,
+            showFilters: null == showFilters
+                ? _value.showFilters
+                : showFilters // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            error: freezed == error
+                ? _value.error
+                : error // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            userLat: freezed == userLat
+                ? _value.userLat
+                : userLat // ignore: cast_nullable_to_non_nullable
+                      as double?,
+            userLng: freezed == userLng
+                ? _value.userLng
+                : userLng // ignore: cast_nullable_to_non_nullable
+                      as double?,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$BloodRequestsStateImplCopyWith<$Res>
+    implements $BloodRequestsStateCopyWith<$Res> {
+  factory _$$BloodRequestsStateImplCopyWith(
+    _$BloodRequestsStateImpl value,
+    $Res Function(_$BloodRequestsStateImpl) then,
+  ) = __$$BloodRequestsStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    List<BloodRequest> requests,
+    List<BloodRequest> filtered,
+    bool hasMore,
+    bool isLoading,
+    bool isLoadingMore,
+    String search,
+    String selectedBloodGroup,
+    String selectedUrgency,
+    bool showFilters,
+    String? error,
+    double? userLat,
+    double? userLng,
+  });
+}
+
+/// @nodoc
+class __$$BloodRequestsStateImplCopyWithImpl<$Res>
+    extends _$BloodRequestsStateCopyWithImpl<$Res, _$BloodRequestsStateImpl>
+    implements _$$BloodRequestsStateImplCopyWith<$Res> {
+  __$$BloodRequestsStateImplCopyWithImpl(
+    _$BloodRequestsStateImpl _value,
+    $Res Function(_$BloodRequestsStateImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of BloodRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? requests = null,
+    Object? filtered = null,
+    Object? hasMore = null,
+    Object? isLoading = null,
+    Object? isLoadingMore = null,
+    Object? search = null,
+    Object? selectedBloodGroup = null,
+    Object? selectedUrgency = null,
+    Object? showFilters = null,
+    Object? error = freezed,
+    Object? userLat = freezed,
+    Object? userLng = freezed,
+  }) {
+    return _then(
+      _$BloodRequestsStateImpl(
+        requests: null == requests
+            ? _value._requests
+            : requests // ignore: cast_nullable_to_non_nullable
+                  as List<BloodRequest>,
+        filtered: null == filtered
+            ? _value._filtered
+            : filtered // ignore: cast_nullable_to_non_nullable
+                  as List<BloodRequest>,
+        hasMore: null == hasMore
+            ? _value.hasMore
+            : hasMore // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        isLoading: null == isLoading
+            ? _value.isLoading
+            : isLoading // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        isLoadingMore: null == isLoadingMore
+            ? _value.isLoadingMore
+            : isLoadingMore // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        search: null == search
+            ? _value.search
+            : search // ignore: cast_nullable_to_non_nullable
+                  as String,
+        selectedBloodGroup: null == selectedBloodGroup
+            ? _value.selectedBloodGroup
+            : selectedBloodGroup // ignore: cast_nullable_to_non_nullable
+                  as String,
+        selectedUrgency: null == selectedUrgency
+            ? _value.selectedUrgency
+            : selectedUrgency // ignore: cast_nullable_to_non_nullable
+                  as String,
+        showFilters: null == showFilters
+            ? _value.showFilters
+            : showFilters // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        error: freezed == error
+            ? _value.error
+            : error // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        userLat: freezed == userLat
+            ? _value.userLat
+            : userLat // ignore: cast_nullable_to_non_nullable
+                  as double?,
+        userLng: freezed == userLng
+            ? _value.userLng
+            : userLng // ignore: cast_nullable_to_non_nullable
+                  as double?,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$BloodRequestsStateImpl extends _BloodRequestsState {
+  const _$BloodRequestsStateImpl({
+    final List<BloodRequest> requests = const <BloodRequest>[],
+    final List<BloodRequest> filtered = const <BloodRequest>[],
+    this.hasMore = true,
+    this.isLoading = true,
+    this.isLoadingMore = false,
+    this.search = '',
+    this.selectedBloodGroup = 'All',
+    this.selectedUrgency = 'All',
+    this.showFilters = false,
+    this.error,
+    this.userLat,
+    this.userLng,
+  }) : _requests = requests,
+       _filtered = filtered,
+       super._();
+
+  final List<BloodRequest> _requests;
+  @override
+  @JsonKey()
+  List<BloodRequest> get requests {
+    if (_requests is EqualUnmodifiableListView) return _requests;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_requests);
+  }
+
+  final List<BloodRequest> _filtered;
+  @override
+  @JsonKey()
+  List<BloodRequest> get filtered {
+    if (_filtered is EqualUnmodifiableListView) return _filtered;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_filtered);
+  }
+
+  @override
+  @JsonKey()
+  final bool hasMore;
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  @JsonKey()
+  final bool isLoadingMore;
+  @override
+  @JsonKey()
+  final String search;
+  @override
+  @JsonKey()
+  final String selectedBloodGroup;
+  @override
+  @JsonKey()
+  final String selectedUrgency;
+  @override
+  @JsonKey()
+  final bool showFilters;
+  @override
+  final String? error;
+  @override
+  final double? userLat;
+  @override
+  final double? userLng;
+
+  @override
+  String toString() {
+    return 'BloodRequestsState(requests: $requests, filtered: $filtered, hasMore: $hasMore, isLoading: $isLoading, isLoadingMore: $isLoadingMore, search: $search, selectedBloodGroup: $selectedBloodGroup, selectedUrgency: $selectedUrgency, showFilters: $showFilters, error: $error, userLat: $userLat, userLng: $userLng)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$BloodRequestsStateImpl &&
+            const DeepCollectionEquality().equals(other._requests, _requests) &&
+            const DeepCollectionEquality().equals(other._filtered, _filtered) &&
+            (identical(other.hasMore, hasMore) || other.hasMore == hasMore) &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.isLoadingMore, isLoadingMore) ||
+                other.isLoadingMore == isLoadingMore) &&
+            (identical(other.search, search) || other.search == search) &&
+            (identical(other.selectedBloodGroup, selectedBloodGroup) ||
+                other.selectedBloodGroup == selectedBloodGroup) &&
+            (identical(other.selectedUrgency, selectedUrgency) ||
+                other.selectedUrgency == selectedUrgency) &&
+            (identical(other.showFilters, showFilters) ||
+                other.showFilters == showFilters) &&
+            (identical(other.error, error) || other.error == error) &&
+            (identical(other.userLat, userLat) || other.userLat == userLat) &&
+            (identical(other.userLng, userLng) || other.userLng == userLng));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    const DeepCollectionEquality().hash(_requests),
+    const DeepCollectionEquality().hash(_filtered),
+    hasMore,
+    isLoading,
+    isLoadingMore,
+    search,
+    selectedBloodGroup,
+    selectedUrgency,
+    showFilters,
+    error,
+    userLat,
+    userLng,
+  );
+
+  /// Create a copy of BloodRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$BloodRequestsStateImplCopyWith<_$BloodRequestsStateImpl> get copyWith =>
+      __$$BloodRequestsStateImplCopyWithImpl<_$BloodRequestsStateImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _BloodRequestsState extends BloodRequestsState {
+  const factory _BloodRequestsState({
+    final List<BloodRequest> requests,
+    final List<BloodRequest> filtered,
+    final bool hasMore,
+    final bool isLoading,
+    final bool isLoadingMore,
+    final String search,
+    final String selectedBloodGroup,
+    final String selectedUrgency,
+    final bool showFilters,
+    final String? error,
+    final double? userLat,
+    final double? userLng,
+  }) = _$BloodRequestsStateImpl;
+  const _BloodRequestsState._() : super._();
+
+  @override
+  List<BloodRequest> get requests;
+  @override
+  List<BloodRequest> get filtered;
+  @override
+  bool get hasMore;
+  @override
+  bool get isLoading;
+  @override
+  bool get isLoadingMore;
+  @override
+  String get search;
+  @override
+  String get selectedBloodGroup;
+  @override
+  String get selectedUrgency;
+  @override
+  bool get showFilters;
+  @override
+  String? get error;
+  @override
+  double? get userLat;
+  @override
+  double? get userLng;
+
+  /// Create a copy of BloodRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$BloodRequestsStateImplCopyWith<_$BloodRequestsStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
