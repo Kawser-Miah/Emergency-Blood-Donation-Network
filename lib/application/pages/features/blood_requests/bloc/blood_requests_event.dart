4a9fb77cbd2ca6b136844b85b5d381b3269adf66
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-07 13:41:45 +0600

    Add BloodRequestsEvent class with various event factories for handling blood request actions

---
+++ lib/application/pages/features/blood_requests/bloc/blood_requests_event.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'blood_requests_event.freezed.dart';
+
+@freezed
+class BloodRequestsEvent with _$BloodRequestsEvent {
+  const factory BloodRequestsEvent.started() = _Started;
+  const factory BloodRequestsEvent.refreshed() = _Refreshed;
+  const factory BloodRequestsEvent.loadMoreRequested() = _LoadMoreRequested;
+  const factory BloodRequestsEvent.searchChanged(String value) = _SearchChanged;
+  const factory BloodRequestsEvent.bloodGroupSelected(String value) = _BloodGroupSelected;
+  const factory BloodRequestsEvent.urgencySelected(String value) = _UrgencySelected;
+  const factory BloodRequestsEvent.filtersOpened() = _FiltersOpened;
+  const factory BloodRequestsEvent.filtersClosed() = _FiltersClosed;
+  const factory BloodRequestsEvent.filtersReset() = _FiltersReset;
+}
