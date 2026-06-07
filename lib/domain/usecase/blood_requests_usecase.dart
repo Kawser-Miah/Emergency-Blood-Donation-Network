8a7fecf6f8b42a64fc986f64fdff7ef783d7e968
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-07 13:42:56 +0600

    Add BloodRequestsUseCase to handle active blood request retrieval

---
+++ lib/domain/usecase/blood_requests_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:blood_setu/domain/models/blood_request.dart';
+import 'package:blood_setu/domain/repositories/blood_request_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+import '../failures/failures.dart';
+
+@injectable
+class BloodRequestsUseCase {
+  final BloodRequestRepository _repository;
+
+  BloodRequestsUseCase(this._repository);
+
+  Future<Either<Failure, List<BloodRequest>>> call({
+    int limit = 20,
+    DateTime? startAfterNeedBy,
+  }) => _repository.getActiveRequests(
+        limit: limit,
+        startAfterNeedBy: startAfterNeedBy,
+      );
+}
