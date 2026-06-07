c4f3025a72321d96f9cfb682150269405a72b622
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-07 20:33:39 +0600

    Remove CloseReason enum from blood_request_enums.dart

--- lib/domain/models/blood_request_enums.dart
+++ lib/domain/models/blood_request_enums.dart
@@ -14,20 +14,3 @@ enum RequestStatus {
     );
   }
 }
-
-enum CloseReason {
-  receivedEnoughDonors('received_enough_donors'),
-  foundAnotherSource('found_another_source'),
-  doctorNoLongerNeeded('doctor_no_longer_needed'),
-  other('other');
-
-  const CloseReason(this.value);
-  final String value;
-
-  static CloseReason fromString(String value) {
-    return CloseReason.values.firstWhere(
-      (e) => e.value == value,
-      orElse: () => CloseReason.other,
-    );
-  }
-}
