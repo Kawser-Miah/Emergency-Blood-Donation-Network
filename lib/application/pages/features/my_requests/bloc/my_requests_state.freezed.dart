92d71fd158ffb53c10a89e0ab7a34d98dcf9ed3f
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-07 20:35:22 +0600

    Update MyRequestsState to include fields for managing blood requests, interested donors, and operation statuses

---
+++ lib/application/pages/features/my_requests/bloc/my_requests_state.freezed.dart
@@ -0,0 +1,410 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'my_requests_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$MyRequestsState {
+  List<BloodRequest> get requests => throw _privateConstructorUsedError;
+  bool get isLoading => throw _privateConstructorUsedError;
+  String? get error => throw _privateConstructorUsedError;
+  List<InterestedDonor> get interestedDonors =>
+      throw _privateConstructorUsedError;
+  bool get isLoadingDonors => throw _privateConstructorUsedError;
+  String? get activeRequestId => throw _privateConstructorUsedError;
+  bool get isUpdating => throw _privateConstructorUsedError;
+  String? get updateError => throw _privateConstructorUsedError;
+  bool get updateSuccess => throw _privateConstructorUsedError;
+  bool get deleteSuccess => throw _privateConstructorUsedError;
+  bool get deleteFailed => throw _privateConstructorUsedError;
+
+  /// Create a copy of MyRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $MyRequestsStateCopyWith<MyRequestsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MyRequestsStateCopyWith<$Res> {
+  factory $MyRequestsStateCopyWith(
+    MyRequestsState value,
+    $Res Function(MyRequestsState) then,
+  ) = _$MyRequestsStateCopyWithImpl<$Res, MyRequestsState>;
+  @useResult
+  $Res call({
+    List<BloodRequest> requests,
+    bool isLoading,
+    String? error,
+    List<InterestedDonor> interestedDonors,
+    bool isLoadingDonors,
+    String? activeRequestId,
+    bool isUpdating,
+    String? updateError,
+    bool updateSuccess,
+    bool deleteSuccess,
+    bool deleteFailed,
+  });
+}
+
+/// @nodoc
+class _$MyRequestsStateCopyWithImpl<$Res, $Val extends MyRequestsState>
+    implements $MyRequestsStateCopyWith<$Res> {
+  _$MyRequestsStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of MyRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? requests = null,
+    Object? isLoading = null,
+    Object? error = freezed,
+    Object? interestedDonors = null,
+    Object? isLoadingDonors = null,
+    Object? activeRequestId = freezed,
+    Object? isUpdating = null,
+    Object? updateError = freezed,
+    Object? updateSuccess = null,
+    Object? deleteSuccess = null,
+    Object? deleteFailed = null,
+  }) {
+    return _then(
+      _value.copyWith(
+            requests: null == requests
+                ? _value.requests
+                : requests // ignore: cast_nullable_to_non_nullable
+                      as List<BloodRequest>,
+            isLoading: null == isLoading
+                ? _value.isLoading
+                : isLoading // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            error: freezed == error
+                ? _value.error
+                : error // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            interestedDonors: null == interestedDonors
+                ? _value.interestedDonors
+                : interestedDonors // ignore: cast_nullable_to_non_nullable
+                      as List<InterestedDonor>,
+            isLoadingDonors: null == isLoadingDonors
+                ? _value.isLoadingDonors
+                : isLoadingDonors // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            activeRequestId: freezed == activeRequestId
+                ? _value.activeRequestId
+                : activeRequestId // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            isUpdating: null == isUpdating
+                ? _value.isUpdating
+                : isUpdating // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            updateError: freezed == updateError
+                ? _value.updateError
+                : updateError // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            updateSuccess: null == updateSuccess
+                ? _value.updateSuccess
+                : updateSuccess // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            deleteSuccess: null == deleteSuccess
+                ? _value.deleteSuccess
+                : deleteSuccess // ignore: cast_nullable_to_non_nullable
+                      as bool,
+            deleteFailed: null == deleteFailed
+                ? _value.deleteFailed
+                : deleteFailed // ignore: cast_nullable_to_non_nullable
+                      as bool,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$MyRequestsStateImplCopyWith<$Res>
+    implements $MyRequestsStateCopyWith<$Res> {
+  factory _$$MyRequestsStateImplCopyWith(
+    _$MyRequestsStateImpl value,
+    $Res Function(_$MyRequestsStateImpl) then,
+  ) = __$$MyRequestsStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    List<BloodRequest> requests,
+    bool isLoading,
+    String? error,
+    List<InterestedDonor> interestedDonors,
+    bool isLoadingDonors,
+    String? activeRequestId,
+    bool isUpdating,
+    String? updateError,
+    bool updateSuccess,
+    bool deleteSuccess,
+    bool deleteFailed,
+  });
+}
+
+/// @nodoc
+class __$$MyRequestsStateImplCopyWithImpl<$Res>
+    extends _$MyRequestsStateCopyWithImpl<$Res, _$MyRequestsStateImpl>
+    implements _$$MyRequestsStateImplCopyWith<$Res> {
+  __$$MyRequestsStateImplCopyWithImpl(
+    _$MyRequestsStateImpl _value,
+    $Res Function(_$MyRequestsStateImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of MyRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? requests = null,
+    Object? isLoading = null,
+    Object? error = freezed,
+    Object? interestedDonors = null,
+    Object? isLoadingDonors = null,
+    Object? activeRequestId = freezed,
+    Object? isUpdating = null,
+    Object? updateError = freezed,
+    Object? updateSuccess = null,
+    Object? deleteSuccess = null,
+    Object? deleteFailed = null,
+  }) {
+    return _then(
+      _$MyRequestsStateImpl(
+        requests: null == requests
+            ? _value._requests
+            : requests // ignore: cast_nullable_to_non_nullable
+                  as List<BloodRequest>,
+        isLoading: null == isLoading
+            ? _value.isLoading
+            : isLoading // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        error: freezed == error
+            ? _value.error
+            : error // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        interestedDonors: null == interestedDonors
+            ? _value._interestedDonors
+            : interestedDonors // ignore: cast_nullable_to_non_nullable
+                  as List<InterestedDonor>,
+        isLoadingDonors: null == isLoadingDonors
+            ? _value.isLoadingDonors
+            : isLoadingDonors // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        activeRequestId: freezed == activeRequestId
+            ? _value.activeRequestId
+            : activeRequestId // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        isUpdating: null == isUpdating
+            ? _value.isUpdating
+            : isUpdating // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        updateError: freezed == updateError
+            ? _value.updateError
+            : updateError // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        updateSuccess: null == updateSuccess
+            ? _value.updateSuccess
+            : updateSuccess // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        deleteSuccess: null == deleteSuccess
+            ? _value.deleteSuccess
+            : deleteSuccess // ignore: cast_nullable_to_non_nullable
+                  as bool,
+        deleteFailed: null == deleteFailed
+            ? _value.deleteFailed
+            : deleteFailed // ignore: cast_nullable_to_non_nullable
+                  as bool,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$MyRequestsStateImpl implements _MyRequestsState {
+  const _$MyRequestsStateImpl({
+    final List<BloodRequest> requests = const <BloodRequest>[],
+    this.isLoading = true,
+    this.error,
+    final List<InterestedDonor> interestedDonors = const <InterestedDonor>[],
+    this.isLoadingDonors = false,
+    this.activeRequestId,
+    this.isUpdating = false,
+    this.updateError,
+    this.updateSuccess = false,
+    this.deleteSuccess = false,
+    this.deleteFailed = false,
+  }) : _requests = requests,
+       _interestedDonors = interestedDonors;
+
+  final List<BloodRequest> _requests;
+  @override
+  @JsonKey()
+  List<BloodRequest> get requests {
+    if (_requests is EqualUnmodifiableListView) return _requests;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_requests);
+  }
+
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  final String? error;
+  final List<InterestedDonor> _interestedDonors;
+  @override
+  @JsonKey()
+  List<InterestedDonor> get interestedDonors {
+    if (_interestedDonors is EqualUnmodifiableListView)
+      return _interestedDonors;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_interestedDonors);
+  }
+
+  @override
+  @JsonKey()
+  final bool isLoadingDonors;
+  @override
+  final String? activeRequestId;
+  @override
+  @JsonKey()
+  final bool isUpdating;
+  @override
+  final String? updateError;
+  @override
+  @JsonKey()
+  final bool updateSuccess;
+  @override
+  @JsonKey()
+  final bool deleteSuccess;
+  @override
+  @JsonKey()
+  final bool deleteFailed;
+
+  @override
+  String toString() {
+    return 'MyRequestsState(requests: $requests, isLoading: $isLoading, error: $error, interestedDonors: $interestedDonors, isLoadingDonors: $isLoadingDonors, activeRequestId: $activeRequestId, isUpdating: $isUpdating, updateError: $updateError, updateSuccess: $updateSuccess, deleteSuccess: $deleteSuccess, deleteFailed: $deleteFailed)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$MyRequestsStateImpl &&
+            const DeepCollectionEquality().equals(other._requests, _requests) &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.error, error) || other.error == error) &&
+            const DeepCollectionEquality().equals(
+              other._interestedDonors,
+              _interestedDonors,
+            ) &&
+            (identical(other.isLoadingDonors, isLoadingDonors) ||
+                other.isLoadingDonors == isLoadingDonors) &&
+            (identical(other.activeRequestId, activeRequestId) ||
+                other.activeRequestId == activeRequestId) &&
+            (identical(other.isUpdating, isUpdating) ||
+                other.isUpdating == isUpdating) &&
+            (identical(other.updateError, updateError) ||
+                other.updateError == updateError) &&
+            (identical(other.updateSuccess, updateSuccess) ||
+                other.updateSuccess == updateSuccess) &&
+            (identical(other.deleteSuccess, deleteSuccess) ||
+                other.deleteSuccess == deleteSuccess) &&
+            (identical(other.deleteFailed, deleteFailed) ||
+                other.deleteFailed == deleteFailed));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    const DeepCollectionEquality().hash(_requests),
+    isLoading,
+    error,
+    const DeepCollectionEquality().hash(_interestedDonors),
+    isLoadingDonors,
+    activeRequestId,
+    isUpdating,
+    updateError,
+    updateSuccess,
+    deleteSuccess,
+    deleteFailed,
+  );
+
+  /// Create a copy of MyRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MyRequestsStateImplCopyWith<_$MyRequestsStateImpl> get copyWith =>
+      __$$MyRequestsStateImplCopyWithImpl<_$MyRequestsStateImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _MyRequestsState implements MyRequestsState {
+  const factory _MyRequestsState({
+    final List<BloodRequest> requests,
+    final bool isLoading,
+    final String? error,
+    final List<InterestedDonor> interestedDonors,
+    final bool isLoadingDonors,
+    final String? activeRequestId,
+    final bool isUpdating,
+    final String? updateError,
+    final bool updateSuccess,
+    final bool deleteSuccess,
+    final bool deleteFailed,
+  }) = _$MyRequestsStateImpl;
+
+  @override
+  List<BloodRequest> get requests;
+  @override
+  bool get isLoading;
+  @override
+  String? get error;
+  @override
+  List<InterestedDonor> get interestedDonors;
+  @override
+  bool get isLoadingDonors;
+  @override
+  String? get activeRequestId;
+  @override
+  bool get isUpdating;
+  @override
+  String? get updateError;
+  @override
+  bool get updateSuccess;
+  @override
+  bool get deleteSuccess;
+  @override
+  bool get deleteFailed;
+
+  /// Create a copy of MyRequestsState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$MyRequestsStateImplCopyWith<_$MyRequestsStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
