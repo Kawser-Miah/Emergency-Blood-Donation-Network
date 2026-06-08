ef750714bb71a8f54fbaeb11e16ba9f05ad94739
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-08 11:47:38 +0600

    Add GetMyInterestsUseCase to retrieve user's interests from BloodRequestRepository

---
+++ lib/domain/usecase/get_my_interests_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:blood_setu/domain/models/blood_request.dart';
+import 'package:blood_setu/domain/repositories/blood_request_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+import '../failures/failures.dart';
+
+@injectable
+class GetMyInterestsUseCase {
+  const GetMyInterestsUseCase(this._repository);
+  final BloodRequestRepository _repository;
+
+  Future<Either<Failure, List<BloodRequest>>> call(String donorUid) =>
+      _repository.getMyInterests(donorUid);
+}
