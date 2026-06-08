3a3c9e55544a1d917a492eb30646f7f839025ef3
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-08 11:45:28 +0600

    Add MyInterestsEvent class for event management in MyInterests feature

---
+++ lib/application/pages/features/my_interests/bloc/my_interests_event.freezed.dart
@@ -0,0 +1,459 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'my_interests_event.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$MyInterestsEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() started,
+    required TResult Function() refreshed,
+    required TResult Function(String requestId) withdrawRequested,
+  }) => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? started,
+    TResult? Function()? refreshed,
+    TResult? Function(String requestId)? withdrawRequested,
+  }) => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? started,
+    TResult Function()? refreshed,
+    TResult Function(String requestId)? withdrawRequested,
+    required TResult orElse(),
+  }) => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Started value) started,
+    required TResult Function(_Refreshed value) refreshed,
+    required TResult Function(_WithdrawRequested value) withdrawRequested,
+  }) => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Started value)? started,
+    TResult? Function(_Refreshed value)? refreshed,
+    TResult? Function(_WithdrawRequested value)? withdrawRequested,
+  }) => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Started value)? started,
+    TResult Function(_Refreshed value)? refreshed,
+    TResult Function(_WithdrawRequested value)? withdrawRequested,
+    required TResult orElse(),
+  }) => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MyInterestsEventCopyWith<$Res> {
+  factory $MyInterestsEventCopyWith(
+    MyInterestsEvent value,
+    $Res Function(MyInterestsEvent) then,
+  ) = _$MyInterestsEventCopyWithImpl<$Res, MyInterestsEvent>;
+}
+
+/// @nodoc
+class _$MyInterestsEventCopyWithImpl<$Res, $Val extends MyInterestsEvent>
+    implements $MyInterestsEventCopyWith<$Res> {
+  _$MyInterestsEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$StartedImplCopyWith<$Res> {
+  factory _$$StartedImplCopyWith(
+    _$StartedImpl value,
+    $Res Function(_$StartedImpl) then,
+  ) = __$$StartedImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$StartedImplCopyWithImpl<$Res>
+    extends _$MyInterestsEventCopyWithImpl<$Res, _$StartedImpl>
+    implements _$$StartedImplCopyWith<$Res> {
+  __$$StartedImplCopyWithImpl(
+    _$StartedImpl _value,
+    $Res Function(_$StartedImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$StartedImpl implements _Started {
+  const _$StartedImpl();
+
+  @override
+  String toString() {
+    return 'MyInterestsEvent.started()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$StartedImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() started,
+    required TResult Function() refreshed,
+    required TResult Function(String requestId) withdrawRequested,
+  }) {
+    return started();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? started,
+    TResult? Function()? refreshed,
+    TResult? Function(String requestId)? withdrawRequested,
+  }) {
+    return started?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? started,
+    TResult Function()? refreshed,
+    TResult Function(String requestId)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (started != null) {
+      return started();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Started value) started,
+    required TResult Function(_Refreshed value) refreshed,
+    required TResult Function(_WithdrawRequested value) withdrawRequested,
+  }) {
+    return started(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Started value)? started,
+    TResult? Function(_Refreshed value)? refreshed,
+    TResult? Function(_WithdrawRequested value)? withdrawRequested,
+  }) {
+    return started?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Started value)? started,
+    TResult Function(_Refreshed value)? refreshed,
+    TResult Function(_WithdrawRequested value)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (started != null) {
+      return started(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Started implements MyInterestsEvent {
+  const factory _Started() = _$StartedImpl;
+}
+
+/// @nodoc
+abstract class _$$RefreshedImplCopyWith<$Res> {
+  factory _$$RefreshedImplCopyWith(
+    _$RefreshedImpl value,
+    $Res Function(_$RefreshedImpl) then,
+  ) = __$$RefreshedImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$RefreshedImplCopyWithImpl<$Res>
+    extends _$MyInterestsEventCopyWithImpl<$Res, _$RefreshedImpl>
+    implements _$$RefreshedImplCopyWith<$Res> {
+  __$$RefreshedImplCopyWithImpl(
+    _$RefreshedImpl _value,
+    $Res Function(_$RefreshedImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$RefreshedImpl implements _Refreshed {
+  const _$RefreshedImpl();
+
+  @override
+  String toString() {
+    return 'MyInterestsEvent.refreshed()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$RefreshedImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() started,
+    required TResult Function() refreshed,
+    required TResult Function(String requestId) withdrawRequested,
+  }) {
+    return refreshed();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? started,
+    TResult? Function()? refreshed,
+    TResult? Function(String requestId)? withdrawRequested,
+  }) {
+    return refreshed?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? started,
+    TResult Function()? refreshed,
+    TResult Function(String requestId)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (refreshed != null) {
+      return refreshed();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Started value) started,
+    required TResult Function(_Refreshed value) refreshed,
+    required TResult Function(_WithdrawRequested value) withdrawRequested,
+  }) {
+    return refreshed(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Started value)? started,
+    TResult? Function(_Refreshed value)? refreshed,
+    TResult? Function(_WithdrawRequested value)? withdrawRequested,
+  }) {
+    return refreshed?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Started value)? started,
+    TResult Function(_Refreshed value)? refreshed,
+    TResult Function(_WithdrawRequested value)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (refreshed != null) {
+      return refreshed(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Refreshed implements MyInterestsEvent {
+  const factory _Refreshed() = _$RefreshedImpl;
+}
+
+/// @nodoc
+abstract class _$$WithdrawRequestedImplCopyWith<$Res> {
+  factory _$$WithdrawRequestedImplCopyWith(
+    _$WithdrawRequestedImpl value,
+    $Res Function(_$WithdrawRequestedImpl) then,
+  ) = __$$WithdrawRequestedImplCopyWithImpl<$Res>;
+  @useResult
+  $Res call({String requestId});
+}
+
+/// @nodoc
+class __$$WithdrawRequestedImplCopyWithImpl<$Res>
+    extends _$MyInterestsEventCopyWithImpl<$Res, _$WithdrawRequestedImpl>
+    implements _$$WithdrawRequestedImplCopyWith<$Res> {
+  __$$WithdrawRequestedImplCopyWithImpl(
+    _$WithdrawRequestedImpl _value,
+    $Res Function(_$WithdrawRequestedImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({Object? requestId = null}) {
+    return _then(
+      _$WithdrawRequestedImpl(
+        null == requestId
+            ? _value.requestId
+            : requestId // ignore: cast_nullable_to_non_nullable
+                  as String,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$WithdrawRequestedImpl implements _WithdrawRequested {
+  const _$WithdrawRequestedImpl(this.requestId);
+
+  @override
+  final String requestId;
+
+  @override
+  String toString() {
+    return 'MyInterestsEvent.withdrawRequested(requestId: $requestId)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$WithdrawRequestedImpl &&
+            (identical(other.requestId, requestId) ||
+                other.requestId == requestId));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, requestId);
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$WithdrawRequestedImplCopyWith<_$WithdrawRequestedImpl> get copyWith =>
+      __$$WithdrawRequestedImplCopyWithImpl<_$WithdrawRequestedImpl>(
+        this,
+        _$identity,
+      );
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() started,
+    required TResult Function() refreshed,
+    required TResult Function(String requestId) withdrawRequested,
+  }) {
+    return withdrawRequested(requestId);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? started,
+    TResult? Function()? refreshed,
+    TResult? Function(String requestId)? withdrawRequested,
+  }) {
+    return withdrawRequested?.call(requestId);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? started,
+    TResult Function()? refreshed,
+    TResult Function(String requestId)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (withdrawRequested != null) {
+      return withdrawRequested(requestId);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Started value) started,
+    required TResult Function(_Refreshed value) refreshed,
+    required TResult Function(_WithdrawRequested value) withdrawRequested,
+  }) {
+    return withdrawRequested(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Started value)? started,
+    TResult? Function(_Refreshed value)? refreshed,
+    TResult? Function(_WithdrawRequested value)? withdrawRequested,
+  }) {
+    return withdrawRequested?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Started value)? started,
+    TResult Function(_Refreshed value)? refreshed,
+    TResult Function(_WithdrawRequested value)? withdrawRequested,
+    required TResult orElse(),
+  }) {
+    if (withdrawRequested != null) {
+      return withdrawRequested(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _WithdrawRequested implements MyInterestsEvent {
+  const factory _WithdrawRequested(final String requestId) =
+      _$WithdrawRequestedImpl;
+
+  String get requestId;
+
+  /// Create a copy of MyInterestsEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$WithdrawRequestedImplCopyWith<_$WithdrawRequestedImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
