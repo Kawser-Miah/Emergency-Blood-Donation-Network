ee3ac768218591f3be52d9499068890446ed883d
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-08 12:34:01 +0600

    Refactor GetMyInterestsUseCase to update return type from List<BloodRequest> to List<MyInterestEntry>

--- lib/domain/usecase/get_my_interests_usecase.dart
+++ lib/domain/usecase/get_my_interests_usecase.dart
@@ -1,4 +1,4 @@
-import 'package:blood_setu/domain/models/blood_request.dart';
+import 'package:blood_setu/domain/models/my_interest_entry.dart';
 import 'package:blood_setu/domain/repositories/blood_request_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
@@ -10,6 +10,6 @@ class GetMyInterestsUseCase {
   const GetMyInterestsUseCase(this._repository);
   final BloodRequestRepository _repository;
 
-  Future<Either<Failure, List<BloodRequest>>> call(String donorUid) =>
+  Future<Either<Failure, List<MyInterestEntry>>> call(String donorUid) =>
       _repository.getMyInterests(donorUid);
 }
