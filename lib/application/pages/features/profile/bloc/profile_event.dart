e56645e1aac07f7732018511db07ef4ffba2e198
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-09 12:59:48 +0600

    Add 'started' event to ProfileEvent for initialization handling

--- lib/application/pages/features/profile/bloc/profile_event.dart
+++ lib/application/pages/features/profile/bloc/profile_event.dart
@@ -4,6 +4,7 @@ part 'profile_event.freezed.dart';
 
 @freezed
 class ProfileEvent with _$ProfileEvent {
+  const factory ProfileEvent.started() = _Started;
   const factory ProfileEvent.infoExpandedToggled() = _InfoExpandedToggled;
   const factory ProfileEvent.notificationsToggled() = _NotificationsToggled;
   const factory ProfileEvent.darkModeToggled() = _DarkModeToggled;
