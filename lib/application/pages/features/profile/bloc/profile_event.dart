eded4753816c95a5c6e34519d669637012dfcc17
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-09 18:39:09 +0600

    Add donationSubmitted event to ProfileEvent for handling donation submissions

--- lib/application/pages/features/profile/bloc/profile_event.dart
+++ lib/application/pages/features/profile/bloc/profile_event.dart
@@ -9,4 +9,9 @@ class ProfileEvent with _$ProfileEvent {
   const factory ProfileEvent.notificationsToggled() = _NotificationsToggled;
   const factory ProfileEvent.darkModeToggled() = _DarkModeToggled;
   const factory ProfileEvent.quietHoursToggled() = _QuietHoursToggled;
+  const factory ProfileEvent.donationSubmitted({
+    required String hospital,
+    required String bloodGroup,
+    required DateTime date,
+  }) = _DonationSubmitted;
 }
