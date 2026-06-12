919aae4da15a2779b5a7dcb4d332746bb8b51bd2
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-12 20:05:29 +0600

    Add FirebaseDatabase to dependency injection in RegisterModule

--- lib/di/register_module.dart
+++ lib/di/register_module.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,6 +12,9 @@ abstract class RegisterModule {
 
   @singleton
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
+
+  @singleton
+  FirebaseDatabase get firebaseDatabase => FirebaseDatabase.instance;
 
   // @singleton
   // FirebaseStorage get fireBaseStorage => FirebaseStorage.instance;
