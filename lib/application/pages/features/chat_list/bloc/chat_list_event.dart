34528513223dc7fb3518e07c97fe8fc9da0c3391
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-12 10:47:54 +0600

    dlt

--- lib/application/pages/features/chat_list/bloc/chat_list_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'chat_list_event.freezed.dart';
-
-@freezed
-class ChatListEvent with _$ChatListEvent {
-  const factory ChatListEvent.searchChanged(String value) = _SearchChanged;
-}
