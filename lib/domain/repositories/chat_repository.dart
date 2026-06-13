9ba292ce10056574212ac1283921fa8690c79a97
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-06-13 12:38:41 +0600

    Add IChatRepository to define conversation, messaging, and presence functionality

---
+++ lib/domain/repositories/chat_repository.dart
@@ -0,0 +1,34 @@
+import '../models/chat_source.dart';
+import '../models/conversation.dart';
+import '../models/message.dart';
+import '../models/message_type.dart';
+import '../models/presence_status.dart';
+
+abstract class IChatRepository {
+  Future<Conversation> getOrCreateConversation({
+    required String currentUid,
+    required String otherUid,
+    required ChatSource chatSource,
+  });
+
+  Stream<List<Conversation>> watchConversations(String uid);
+
+  Stream<List<Message>> watchMessages(String conversationId);
+
+  Future<void> sendMessage({
+    required String conversationId,
+    required String senderId,
+    required String text,
+    required MessageType type,
+  });
+
+  Future<void> markAsRead({
+    required String conversationId,
+    required String uid,
+  });
+
+  Future<void> setOnlineStatus(String uid, bool online);
+
+  /// Streams the presence (online flag + lastSeen) of another user from RTDB.
+  Stream<PresenceStatus> watchPresence(String uid);
+}
